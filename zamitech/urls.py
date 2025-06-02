e9f35ddf8364401ce9be8428408962950e895a12
Mahdieh Kazemi <136577439+mahkzmi@users.noreply.github.com>
2025-06-02 22:44:25 +0330

    Update urls.py

--- zamitech/urls.py
+++ zamitech/urls.py
@@ -1,29 +1,24 @@
-"""
-URL configuration for zamitech project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.conf import settings
-from django.conf.urls.static import static
+from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 import os
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('admin/', admin.site.urls),
+
+    # صفحات اصلی
+    path('', TemplateView.as_view(template_name="index.html"), name='home'),
+    path('about-us/', TemplateView.as_view(template_name="about-us.html"), name='about'),
+    path('contact-us/', TemplateView.as_view(template_name="contact-us.html"), name='contact'),
+    path('product/', TemplateView.as_view(template_name="product.html"), name='product'),
+    path('request-demo/', TemplateView.as_view(template_name="request-demo.html"), name='demo'),
 ]
 
-
+# برای سرویس‌دهی به فایل‌های استاتیک در حالت توسعه (development)
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=os.path.join(settings.BASE_DIR, 'frontend', 'static'))
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=os.path.join(settings.BASE_DIR, 'frontend', 'static')
+    )
