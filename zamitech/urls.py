47e63d505e4259112a26dfa20634953638537d03
ghaem <ghaemaghaey@gmail.com>
2025-07-14 10:51:47 +0330

    re linking everything and removing hard links

--- zamitech/urls.py
+++ zamitech/urls.py
@@ -6,19 +6,30 @@ from django.conf.urls.static import static
 import os
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
+    path("admin/", admin.site.urls),
     # صفحات اصلی
-    path('', TemplateView.as_view(template_name="index.html"), name='home'),
-    path('about-us/', TemplateView.as_view(template_name="about-us.html"), name='about'),
-    path('contact-us/', TemplateView.as_view(template_name="contact-us.html"), name='contact'),
-    path('product/', TemplateView.as_view(template_name="product.html"), name='product'),
-    path('request-demo/', TemplateView.as_view(template_name="request-demo.html"), name='demo'),
+    path("", TemplateView.as_view(template_name="index.html"), name="home"),
+    path(
+        "about-us/", TemplateView.as_view(template_name="about-us.html"), name="about"
+    ),
+    path(
+        "contact-us/",
+        TemplateView.as_view(template_name="contact-us.html"),
+        name="contact",
+    ),
+    path(
+        "product/", TemplateView.as_view(template_name="product.html"), name="product"
+    ),
+    path(
+        "request-demo/",
+        TemplateView.as_view(template_name="request-demo.html"),
+        name="demo",
+    ),
 ]
 
 # برای سرویس‌دهی به فایل‌های استاتیک در حالت توسعه (development)
 if settings.DEBUG:
     urlpatterns += static(
         settings.STATIC_URL,
-        document_root=os.path.join(settings.BASE_DIR, 'frontend', 'static')
+        document_root=os.path.join(settings.BASE_DIR, "frontend", "static"),
     )
